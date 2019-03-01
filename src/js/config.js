f82cb81731e18d263067e3b871ccc8221acf44e9
rainbowmoonlight.github.io <46019037+rainbowmoonlight@users.noreply.github.com>
2019-02-28 23:04:00 -0800

    update

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,5 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = '462b1cc8d4f2730081462fbc65136320';
+export const key = '58eda3bc8e2891c1342165b902c30f95';
+
+//58eda3bc8e2891c1342165b902c30f95
+//462b1cc8d4f2730081462fbc65136320
